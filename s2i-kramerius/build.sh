4ce22652476fc2b3b1b5f53243b7a5957ea4c320
Jakub Kremláček <jkremlacek@email.cz>
2018-07-10 08:42:16 +0200

    Tag normalization

--- s2i-kramerius/build.sh
+++ s2i-kramerius/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker build -t kramerius-builder .
-s2i build --ref=fcrepo4 --incremental=true --exclude="" https://github.com/ceskaexpedice/kramerius.git kramerius-builder moravianlibrary/kramerius
+docker build -t kramerius-builder6 .
+s2i build --ref=fcrepo4 --incremental=true --exclude="" https://github.com/ceskaexpedice/kramerius.git kramerius6-builder moravianlibrary/kramerius6
 if hash docker-squash 2>/dev/null; then
-  docker-squash moravianlibrary/kramerius -t moravianlibrary/kramerius
+  docker-squash moravianlibrary/kramerius6 -t moravianlibrary/kramerius6
 fi
